b9249aead8c4f1b01b9a5762dab0468b07cb2b29
Noah Sanchez <alu0101134956@ull.edu.es>
2022-02-01 01:38:05 +0000

    Adding Footer component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <H1>Noah Sanchez</H1>
+  <Footer/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Footer
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Zen+Kurenaido&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Zen Kurenaido', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: white;
   margin-top: 60px;
+}
+
+body {
+  background: #202023;
 }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <span>&copy; Noah Sanchez {{ new Date().getFullYear() }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style scoped>
+.container {
+  width: 300px;
+  margin: auto;
+  font-size: 25px;
+}
+</style>
